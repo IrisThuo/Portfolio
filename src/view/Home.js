cba8a8de0a824c6f567737c6fce91baa3e5e3f7a
Iris <iris.thuo@student.moringaschool.com>
2023-06-20 17:43:26 +0300

    home page

---
+++ src/view/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <div>
+            <h1>IRIS THUO</h1>
+            <h1>FULL STACK WEB DEVELOPER</h1>
+
+            <button>REACH ME</button>
+            <button>PORTFOLIO</button>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
